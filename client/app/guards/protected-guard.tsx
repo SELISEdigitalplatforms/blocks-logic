98622a816853b3c749620e0fa5cab65d1ca7b4f6
afiapreety <afia.preety@selisegroup.com>
2026-04-09 17:37:30 +0600

    fix: auth guards

--- client/app/guards/protected-guard.tsx
+++ client/app/guards/protected-guard.tsx
@@ -1,21 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuthStore } from "@/store/useAuthStore";
+import { useAppState } from "./public-guard";
 
 export function ProtectedGuard({ children }: { children: React.ReactNode }) {
   const { isAuthenticated } = useAuthStore();
-  const [isMounted, setIsMounted] = useState(false);
+  const { isMounted } = useAppState();
   const navigate = useNavigate();
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
+  
   useEffect(() => {
     if (!isMounted) return;
-    if (!isAuthenticated) {
-      navigate("/login", { replace: true });
-    }
+    if (!isAuthenticated) return navigate("/login", { replace: true });
   }, [isAuthenticated, isMounted, navigate]);
 
   if (!isMounted || !isAuthenticated) return null;
