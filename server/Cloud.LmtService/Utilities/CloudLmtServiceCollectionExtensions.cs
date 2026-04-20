c4013857cbfbda95ea029fe71f4793501dca93f6
abdullahmomen2037 <abdullah.momen@selisegroup.com>
2026-04-20 14:56:30 +0600

    Log feature implemented

---
+++ server/Cloud.LmtService/Utilities/CloudLmtServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Cloud.LmtService.Repositories.Logs;
+using Cloud.LmtService.Services.Logs;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cloud.LmtService.Utilities
+{
+    public static class CloudLmtServiceCollectionExtensions
+    {
+        public static void AddCloudLmtServices(this IServiceCollection services)
+        {
+            services.AddSingleton<ILogService, LogService>();
+            services.AddSingleton<ILogRepository, LogRepository>();
+        }
+    }
+}
