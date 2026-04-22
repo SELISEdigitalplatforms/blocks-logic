7b24ccc268fbd0d2c3b0a01cedab0c95523110c0
afiapreety <afia.preety@selisegroup.com>
2026-04-22 23:36:04 +0600

    fix: fe build issue

--- client/app/idp/captcha/modals/configure-captcha-modal/utils.ts
+++ client/app/idp/captcha/modals/configure-captcha-modal/utils.ts
@@ -4,9 +4,11 @@ export const ConfigureCaptchaFormSchema = z.object({
   provider: z.enum(["recaptcha", "hcaptcha"]),
   captchaKey: z.string().min(1, "Site key is required"),
   captchaSecret: z.string().min(1, "Secret key is required"),
+  captchaGenerator: z.string().min(1, "Generator type is required"),
 });
 export const ConfigureCaptchaFormDefaultValue = {
   provider: "",
   captchaKey: "",
   captchaSecret: "",
+  captchaGenerator: "",
 };
