5102577b8d3522d3b5e104f8f15d5f9754315824
afiapreety <afia.preety@selisegroup.com>
2026-04-28 15:51:30 +0600

    fix: commenting out oidc token call redirection from FE

--- client/app/routes/oidc/index.tsx
+++ client/app/routes/oidc/index.tsx
@@ -31,7 +31,7 @@ export default function OidcIndexPage() {
         }
         setAuthenticated();
 
-        window.location.href = `${window.location.origin}/console`;
+        // window.location.href = `${window.location.origin}/console`;
       })
       .catch(() => {
         navigate("/oidc/error");
