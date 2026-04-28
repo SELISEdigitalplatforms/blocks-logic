4784babc3cb65dfb97c70ab59ce0076c47d0fedb
afiapreety <afia.preety@selisegroup.com>
2026-04-28 15:26:43 +0600

    feat: client_secret hardcoded for oidc token call

--- client/app/idp/authentication/services/auth.service.ts
+++ client/app/idp/authentication/services/auth.service.ts
@@ -47,6 +47,8 @@ export class AuthService {
     body.append("grant_type", "authorization_code");
     body.append("code", payload.code);
     body.append("state", payload.state);
+    body.append("client_secret", "e048ec1b63d548dd85d053f364d5d54c");
+
     return http.post(AUTH_ENDPOINTS.TOKEN, body, {
       "Content-Type": "application/x-www-form-urlencoded",
     });
