487c84e94708510e128f36e11e3c31cda17ebf54
abdullahmomen2037 <abdullah.momen@selisegroup.com>
2026-05-18 16:01:22 +0600

    service name change

--- server/Worker/Program.cs
+++ server/Worker/Program.cs
@@ -23,7 +23,7 @@ using DomainService.Workflow.Events;
 using DomainService.Workflow.Nodes.TriggerDataV1;
 using Worker.Consumers.Workflow;
 
-const string _serviceName = "blocks-os-worker";
+const string _serviceName = "blocks-logic-worker";
 
 var vaultType = ResolveVaultType();
 Console.WriteLine($"Using Genesis vault type: {vaultType}");
