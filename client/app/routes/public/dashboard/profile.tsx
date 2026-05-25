62a0575d83483b120912de18a2e66905cf4eafa7
nuzattasnim <nuzat.tasnim@selisegroup.com>
2026-05-25 16:57:31 +0600

    fix: update urls

--- client/app/routes/public/dashboard/profile.tsx
+++ client/app/routes/public/dashboard/profile.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 export default function ProfilePage() {
 	useEffect(() => {
-		window.location.href = "http://stg-iam.blocksdevelopers.com/profile";
+		window.location.href = "http://iam.seliseblocks.com/profile";
 	}, []);
 
 	return null;
