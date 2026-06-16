14b005ce5a96684ecb4480d619c13ea66c6ac80d
nuzattasnim <nuzat.tasnim@selisegroup.com>
2026-06-16 15:59:39 +0600

    fix: cleanup unreferenced codes

--- client/app/idp/iam/services/iam.service.ts
+++
@@ -1,9 +0,0 @@
-import { PermissionService } from "./permission.service";
-
-class IAMService {
-  constructor(
-    public permission: PermissionService,
-  ) {}
-}
-
-export const iamService = new IAMService(new PermissionService());
