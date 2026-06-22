f9ce10c7b3e5b238b0cd07db2e219fbfcfee45fc
UDay <uday.dev.bd@gmail.com>
2026-06-23 05:23:35 +0600

    fix: add vrsion

--- server/DomainService/Workflow/Services/IWorkflowService.cs
+++ server/DomainService/Workflow/Services/IWorkflowService.cs
@@ -12,13 +12,15 @@ namespace DomainService.Workflow.Services
         Task<WorkflowGetsResponseDto> GetAllAsync(WorkflowGetsRequestDto dto);
 
         Task<WorkflowGetResponseDto> GetAsync(WorkflowGetRequestDto dto);
-
         Task<BaseMutationResponse> UpdateAsync(WorkflowUpdateRequestDto dto);
-
         Task<BaseMutationResponse> DeleteAsync(WorkflowDeleteRequestDto dto);
 
         Task<BaseMutationResponse> CreateVersion(WorkflowVersionCreateRequestDto dto);
 
         Task<WorkflowGetVersionsResponseDto> GetVersions(WorkflowGetVersionsRequestDto dto);
+
+        Task<BaseMutationResponse> PublishAsync(WorkflowPublishRequestDto dto);
+
+        Task<BaseMutationResponse> RestoreAsync(WorkflowRestoreRequestDto dto);
     }
 }
