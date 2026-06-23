be82945a8ac1ab42af903426470b5632eb44415e
UDay <uday.dev.bd@gmail.com>
2026-06-23 17:03:44 +0600

    feat: implement GetWorkflowByVersion and update workflow versioning methods to async

--- server/DomainService/Workflow/Dtos/WorkflowGetVersionsResponseDto.cs
+++ server/DomainService/Workflow/Dtos/WorkflowGetVersionsResponseDto.cs
@@ -5,7 +5,21 @@ using DomainService.Workflow.Models;
 
 namespace DomainService.Workflow.Dtos
 {
-    public class WorkflowGetVersionsResponseDto : BaseQueryListResponse<List<WorkflowVersionModel>>
+    public class WorkflowGetVersionsResponseDto : BaseQueryListResponse<List<WorkflowGetVersionSummary>>
     {
     }
+
+    public class WorkflowGetVersionSummary
+    {
+        public required string ItemId { get; set; }
+        public required string WorkflowId { get; set; }
+        public required string TenantId { get; set; }
+        public required string Name { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastUpdatedDate { get; set; }
+        public string? CreatedBy { get; set; }
+        public string? LastUpdatedBy { get; set; }
+    }
+
 }
