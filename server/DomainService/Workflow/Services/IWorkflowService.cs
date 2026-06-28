b97ad0501969119eb966d83207859b105fd7ac2e
UDay <uday.dev.bd@gmail.com>
2026-06-28 17:40:31 +0600

    feat: implement PublishNewVersion and PublishVersion methods with corresponding DTOs in WorkflowService

--- server/DomainService/Workflow/Services/IWorkflowService.cs
+++ server/DomainService/Workflow/Services/IWorkflowService.cs
@@ -24,7 +24,9 @@ namespace DomainService.Workflow.Services
 
         Task<GetWorkflowByVersionResponseDto> GetWorkflowByVersionAsync(GetWorkflowByVersionRequestDto dto);
 
-        Task<BaseMutationResponse> PublishAsync(WorkflowPublishRequestDto dto);
+        Task<BaseMutationResponse> PublishNewVersionAsync(WorkflowPublishNewVersionRequestDto dto);
+
+        Task<BaseMutationResponse> PublishVersionAsync(WorkflowPublishVersionRequestDto dto);
 
         Task<BaseMutationResponse> UnpublishAsync(WorkflowUnpublishRequestDto dto);
 
