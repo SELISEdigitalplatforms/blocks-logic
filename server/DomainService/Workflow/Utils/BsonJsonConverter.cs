cd31aea6bb4bc311437ec05a5e00c22df2009c0a
UDay <uday.dev.bd@gmail.com>
2026-07-01 18:20:16 +0600

    fix: resolve step execution

--- server/DomainService/Workflow/Utils/BsonJsonConverter.cs
+++ server/DomainService/Workflow/Utils/BsonJsonConverter.cs
@@ -1,17 +1,29 @@
 using System.Text.Json;
 using MongoDB.Bson;
+using MongoDB.Bson.IO;
 
 namespace DomainService.Workflow.Utils
 {
     public static class BsonJsonConverter
     {
+        private static readonly JsonWriterSettings RelaxedExtendedJsonSettings = new()
+        {
+            OutputMode = JsonOutputMode.RelaxedExtendedJson
+        };
+
         public static JsonElement ToJsonElement(BsonValue bson)
         {
-            var json = bson.ToJson(new MongoDB.Bson.IO.JsonWriterSettings
-            {
-                OutputMode = MongoDB.Bson.IO.JsonOutputMode.RelaxedExtendedJson
-            });
+            var json = bson.ToJson(RelaxedExtendedJsonSettings);
+            using var doc = JsonDocument.Parse(json);
+            return doc.RootElement.Clone();
+        }
 
+        public static JsonElement? ToJsonElementOrNull(BsonValue? bson)
+        {
+            if (bson is null || bson.IsBsonNull)
+                return null;
+
+            var json = bson.ToJson(RelaxedExtendedJsonSettings);
             using var doc = JsonDocument.Parse(json);
             return doc.RootElement.Clone();
         }
@@ -19,9 +31,20 @@ namespace DomainService.Workflow.Utils
         public static BsonValue ToBsonValue(JsonElement element)
         {
             var json = element.GetRawText();
-            return MongoDB.Bson.Serialization.BsonSerializer
-                .Deserialize<BsonValue>(json);
+            return MongoDB.Bson.Serialization.BsonSerializer.Deserialize<BsonValue>(json);
+        }
+
+        public static BsonArray? ToBsonArrayOrNull(JsonElement? element)
+        {
+            if (element is null)
+                return null;
+
+            var kind = element.Value.ValueKind;
+            if (kind == JsonValueKind.Null || kind == JsonValueKind.Undefined)
+                return null;
+
+            var bson = MongoDB.Bson.Serialization.BsonSerializer.Deserialize<BsonValue>(element.Value.GetRawText());
+            return bson as BsonArray;
         }
     }
-
 }
