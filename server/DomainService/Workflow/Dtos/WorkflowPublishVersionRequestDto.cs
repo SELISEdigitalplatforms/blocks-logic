b1f3325a2db83cd73a7491b2c23d720d872bc537
UDay <uday.dev.bd@gmail.com>
2026-07-08 12:03:42 +0600

    Refactor Workflow DTOs and Services to Remove ProjectKey

--- server/DomainService/Workflow/Dtos/WorkflowPublishVersionRequestDto.cs
+++ server/DomainService/Workflow/Dtos/WorkflowPublishVersionRequestDto.cs
@@ -1,13 +1,9 @@
 using System.ComponentModel.DataAnnotations;
-using Blocks.Genesis;
 
 namespace DomainService.Workflow.Dtos
 {
-    public class WorkflowPublishVersionRequestDto : IProjectKey
+    public class WorkflowPublishVersionRequestDto
     {
-        [Required]
-        public required string ProjectKey { get; set; }
-
         [Required]
         public required string WorkflowId { get; set; }
 
