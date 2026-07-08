2d7a2682636ff62dd7bfe0c68e93e729d644cb7d
UDay <uday.dev.bd@gmail.com>
2026-07-08 12:03:42 +0600

    Refactor Workflow DTOs and Services to Remove ProjectKey

--- server/DomainService/Workflow/Dtos/StepExecuteRequestDto.cs
+++ server/DomainService/Workflow/Dtos/StepExecuteRequestDto.cs
@@ -2,7 +2,6 @@ namespace DomainService.Workflow.Dtos
 {
     public class StepExecuteRequestDto
     {
-        public required string ProjectKey { get; set; }
         public required string WorkflowId { get; set; }
         public required string NodeId { get; set; }
         public string? SourceExecutionId { get; set; }
--- server/DomainService/Workflow/Dtos/TriggerListenerRequestDto.cs
+++ server/DomainService/Workflow/Dtos/TriggerListenerRequestDto.cs
@@ -2,7 +2,6 @@ namespace DomainService.Workflow.Dtos
 {
     public class TriggerListenerRequestDto
     {
-        public required string ProjectKey { get; set; }
         public required string WorkflowId { get; set; }
         public string? TriggerId { get; set; }
         public string? CompletionNodeId { get; set; }
