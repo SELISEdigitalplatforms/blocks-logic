5ce9ded493db9448caaa936699b45b60f814359b
nuzattasnim <nuzat.tasnim@selisegroup.com>
2026-07-09 14:22:14 +0600

    fix input panel colon issue

--- client/app/modules/workflow/components/node-inspector/shared/input-panel/components/schema-tab.tsx
+++ client/app/modules/workflow/components/node-inspector/shared/input-panel/components/schema-tab.tsx
@@ -33,7 +33,7 @@ export function SchemaTab({
             hasSinglePredecessor={hasSinglePredecessor}
             showValues={!isLastExecutionEditor}
             isDraggable={!isExecutionMode}
-            showColon={isLastExecutionEditor}
+            showColon={!isLastExecutionEditor}
           />
         </div>
       ))}
