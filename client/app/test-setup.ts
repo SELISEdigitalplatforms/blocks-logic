948dac108a7b70947832b3e9442b15a5919d72f4
rezwanx <rezwanxx@gmail.com>
2026-07-24 11:36:57 +0200

    test(client): cover stepper provider and infinite scroller

--- client/app/test-setup.ts
+++ client/app/test-setup.ts
@@ -52,6 +52,9 @@ if (typeof Element !== "undefined") {
   if (!Element.prototype.scrollIntoView) {
     Element.prototype.scrollIntoView = () => {};
   }
+  if (!Element.prototype.scrollTo) {
+    Element.prototype.scrollTo = () => {};
+  }
   if (!Element.prototype.hasPointerCapture) {
     Element.prototype.hasPointerCapture = () => false;
   }
