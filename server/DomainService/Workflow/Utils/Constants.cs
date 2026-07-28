a45c29508de431bb1ded827aefab8128bfb1801a
theshameem75 <shameem.alam@selisegroup.com>
2026-07-28 19:08:23 +0600

    solved conflict

--- server/DomainService/Workflow/Utils/Constants.cs
+++ server/DomainService/Workflow/Utils/Constants.cs
@@ -7,7 +7,8 @@ namespace DomainService.Workflow.Utils
         public const string NodeExecutionQueue = "blocks_logic_workflow_node_execute_listener";
         public const string EmailTriggerQueue = "blocks_logic_workflow_email_trigger_listener";
         public const string DataTriggerQueue = "blocks_logic_workflow_data_trigger_listener";
-
+        public const string LogicMailQueueName = "blocks_email_listener";
+        public const string MigrationCompletionTopic = "migration_topic";
         public const string AccessTokenCookieName = "access_token";
         public const string RefreshTokenCookieName = "refresh_token";
 
@@ -47,7 +48,9 @@ namespace DomainService.Workflow.Utils
                 {
                     ConsumerSubscriptions = [ConsumerSubscription.BindToQueue(NodeExecutionQueue),
                                              ConsumerSubscription.BindToQueue(EmailTriggerQueue),
-                                             ConsumerSubscription.BindToQueue(DataTriggerQueue)],
+                                             ConsumerSubscription.BindToQueue(DataTriggerQueue),
+                                             ConsumerSubscription.BindToQueue(LogicMailQueueName),
+                                             ConsumerSubscription.BindToQueue(MigrationCompletionTopic)],
 
                 }
             };
@@ -59,8 +62,8 @@ namespace DomainService.Workflow.Utils
             {
                 AzureServiceBusConfiguration = new AzureServiceBusConfiguration
                 {
-                    Queues = [NodeExecutionQueue, EmailTriggerQueue, DataTriggerQueue],
-                    Topics = []
+                    Queues = [NodeExecutionQueue, EmailTriggerQueue, DataTriggerQueue, LogicMailQueueName],
+                    Topics = [MigrationCompletionTopic]
                 }
             };
         }
