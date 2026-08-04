9b5bf99bbf6bf182a2cc1e7797adf76878fd6fc7
abdullahmomen2037 <abdullah.momen@selisegroup.com>
2026-08-04 17:09:22 +0600

    endpoint clean implemented

--- server/Api/Controllers/ProjectController.cs
+++ server/Api/Controllers/ProjectController.cs
@@ -1,4 +1,4 @@
-﻿using Blocks.Genesis;
+using Blocks.Genesis;
 using DomainService.Dtos;
 using DomainService.Entities;
 using DomainService.Projects;
@@ -31,19 +31,19 @@ namespace Api.Controllers
 
         }
 
-        [HttpPost]
-        [Authorize]
-        public async Task<CreateProjectResponse> Create([FromBody] CreateProjectRequest request)
-        {
-            var validationResult = await _createProjectValidator.ValidateAsync(request);
+        //[HttpPost]
+        //[Authorize]
+        //public async Task<CreateProjectResponse> Create([FromBody] CreateProjectRequest request)
+        //{
+        //    var validationResult = await _createProjectValidator.ValidateAsync(request);
 
-            if (!validationResult.IsValid)
-            {
-                return new CreateProjectResponse { Errors = validationResult.Errors.ToDictionary(e => string.IsNullOrWhiteSpace(e.PropertyName) ? "validation_error" : e.PropertyName, e => e.ErrorMessage), IsSuccess = false };
-            }
+        //    if (!validationResult.IsValid)
+        //    {
+        //        return new CreateProjectResponse { Errors = validationResult.Errors.ToDictionary(e => string.IsNullOrWhiteSpace(e.PropertyName) ? "validation_error" : e.PropertyName, e => e.ErrorMessage), IsSuccess = false };
+        //    }
 
-            return await _projectManagementService.SaveProjectAsync(request);
-        }
+        //    return await _projectManagementService.SaveProjectAsync(request);
+        //}
 
 
         [HttpGet]
@@ -53,12 +53,12 @@ namespace Api.Controllers
             return await _projectManagementService.GetAllAsync(request);
         }
 
-        [HttpPost]
-        [Authorize]
-        public async Task<RestoreProjectResponse> Restore([FromBody] RestoreProjectRequest restoreProjectRequest)
-        {
-            return await _projectManagementService.RestoreProjectAsync(restoreProjectRequest);
-        }
+        //[HttpPost]
+        //[Authorize]
+        //public async Task<RestoreProjectResponse> Restore([FromBody] RestoreProjectRequest restoreProjectRequest)
+        //{
+        //    return await _projectManagementService.RestoreProjectAsync(restoreProjectRequest);
+        //}
 
         [HttpGet]
         [Authorize]
@@ -67,43 +67,43 @@ namespace Api.Controllers
             return await _projectManagementService.GetAsync();
         }
 
-        [HttpPost]
-        [Authorize]
-        public async Task<BaseResponse> UpdateProject([FromBody] UpdateProjectRequest request)
-        {
-            var validationResult = await _updateProjectValidator.ValidateAsync(request);
+        //[HttpPost]
+        //[Authorize]
+        //public async Task<BaseResponse> UpdateProject([FromBody] UpdateProjectRequest request)
+        //{
+        //    var validationResult = await _updateProjectValidator.ValidateAsync(request);
 
-            if (!validationResult.IsValid)
-            {
-                return new BaseResponse { IsSuccess = false, Errors = validationResult.Errors.ToDictionary(e => e.PropertyName, e => e.ErrorMessage) };
-            }
+        //    if (!validationResult.IsValid)
+        //    {
+        //        return new BaseResponse { IsSuccess = false, Errors = validationResult.Errors.ToDictionary(e => e.PropertyName, e => e.ErrorMessage) };
+        //    }
 
-            return await _projectManagementService.UpdateProjectAsync(request);
-        }
+        //    return await _projectManagementService.UpdateProjectAsync(request);
+        //}
 
-        [Authorize]
-        [HttpPost]
-        public async Task<BaseResponse> UpdateTenantGroup([FromBody] UpdateTenantGroupRequest request)
-        {
-            if (string.IsNullOrWhiteSpace(request.Name) || string.IsNullOrWhiteSpace(request.TenantGroupId))
-            {
-                return new BaseResponse { IsSuccess = false, Errors = new Dictionary<string, string> { { "property_missing", "TenantGroupId or ProjectNane should not be empty" } } };
-            }
+        //[Authorize]
+        //[HttpPost]
+        //public async Task<BaseResponse> UpdateTenantGroup([FromBody] UpdateTenantGroupRequest request)
+        //{
+        //    if (string.IsNullOrWhiteSpace(request.Name) || string.IsNullOrWhiteSpace(request.TenantGroupId))
+        //    {
+        //        return new BaseResponse { IsSuccess = false, Errors = new Dictionary<string, string> { { "property_missing", "TenantGroupId or ProjectNane should not be empty" } } };
+        //    }
 
-            return await _projectManagementService.UpdateTenantGroupAsync(request);
-        }
+        //    return await _projectManagementService.UpdateTenantGroupAsync(request);
+        //}
 
-        [HttpPost]
-        [Authorize]
-        public async Task<BaseResponse> Disable([FromBody] DisableProjectRequest request)
-        {
-            if (string.IsNullOrWhiteSpace(BlocksContext.GetContext().TenantId))
-            {
-                return new AuthConfigResponse { Errors = new Dictionary<string, string> { { "missing_projectKey", "ProjectKey is required" } } };
-            }
+        //[HttpPost]
+        //[Authorize]
+        //public async Task<BaseResponse> Disable([FromBody] DisableProjectRequest request)
+        //{
+        //    if (string.IsNullOrWhiteSpace(BlocksContext.GetContext().TenantId))
+        //    {
+        //        return new AuthConfigResponse { Errors = new Dictionary<string, string> { { "missing_projectKey", "ProjectKey is required" } } };
+        //    }
 
-            return await _projectManagementService.DisableProjectAsync(BlocksContext.GetContext().TenantId);
-        }
+        //    return await _projectManagementService.DisableProjectAsync(BlocksContext.GetContext().TenantId);
+        //}
 
         [HttpGet]
         [Authorize]
@@ -132,26 +132,26 @@ namespace Api.Controllers
             return await _projectManagementService.UpdateTokenValidationParametersAsync(request);
         }
 
-        [HttpGet]
-        [Authorize]
-        public async Task<IActionResult> GetTokenValidationParameters([FromQuery] GetTokenValidationParametersRequest request)
-        {
-            return await _projectManagementService.GetProjectTokenValidationParametersAsync(BlocksContext.GetContext().TenantId);
-        }
+        //[HttpGet]
+        //[Authorize]
+        //public async Task<IActionResult> GetTokenValidationParameters([FromQuery] GetTokenValidationParametersRequest request)
+        //{
+        //    return await _projectManagementService.GetProjectTokenValidationParametersAsync(BlocksContext.GetContext().TenantId);
+        //}
 
-        [HttpPost]
-        [Authorize]
-        public async Task<SaveThirdPartyJWTClaimsResponse> SaveThirdPartyJWTClaims([FromBody] SaveThirdPartyJWTClaimsRequest request)
-        {
+        //[HttpPost]
+        //[Authorize]
+        //public async Task<SaveThirdPartyJWTClaimsResponse> SaveThirdPartyJWTClaims([FromBody] SaveThirdPartyJWTClaimsRequest request)
+        //{
 
-            return await _projectManagementService.SaveThirdPartyJWTClaimsAsync(request);
-        }
+        //    return await _projectManagementService.SaveThirdPartyJWTClaimsAsync(request);
+        //}
 
-        [HttpGet]
-        [Authorize]
-        public async Task<ThirdPartyJWTClaims?> GetThirdPartyJWTClaims([FromQuery] GetThirdPartyJWTClaimsRequest request)
-        {
-            return await _projectManagementService.GetThirdPartyJWTClaimsAsync(request);
-        }
+        //[HttpGet]
+        //[Authorize]
+        //public async Task<ThirdPartyJWTClaims?> GetThirdPartyJWTClaims([FromQuery] GetThirdPartyJWTClaimsRequest request)
+        //{
+        //    return await _projectManagementService.GetThirdPartyJWTClaimsAsync(request);
+        //}
     }
 }
