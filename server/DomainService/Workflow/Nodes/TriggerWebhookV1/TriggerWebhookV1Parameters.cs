0582b1a8aa9e65550c841ba3332b100b1117a375
UDay <uday.dev.bd@gmail.com>
2026-08-11 11:18:24 +0600

    fix: correct placement of HttpResponseMode property in TriggerWebhookV1Parameters

--- server/DomainService/Workflow/Nodes/TriggerWebhookV1/TriggerWebhookV1Parameters.cs
+++ server/DomainService/Workflow/Nodes/TriggerWebhookV1/TriggerWebhookV1Parameters.cs
@@ -7,11 +7,12 @@ namespace DomainService.Workflow.Nodes.TriggerWebhookV1
         public string Path { get; set; } = string.Empty;
         public string ExecutionMode { get; set; } = "Test";
         public string HttpMethod { get; set; } = "POST";
+        public string HttpResponseMode { get; set; } = "immediate";
         public string AuthType { get; set; } = "none";
         public string OrganizationId { get; set; } = "";
         public WorkflowAuthService.AuthorizationMode? AuthorizationMode { get; set; }
         public WorkflowAuthService.Rule? Roles { get; set; }
-        public WorkflowAuthService.Rule? Permissions { get; set; }        public string HttpResponseMode { get; set; } = "immediate";
+        public WorkflowAuthService.Rule? Permissions { get; set; }
         public string HttpResponseData { get; set; } = "all";
     }
 }
