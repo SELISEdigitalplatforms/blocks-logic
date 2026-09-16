50778158e3011be9b18c1e90b5e595b8bb09eac5
nuzattasnim <nuzat.tasnim@selisegroup.com>
2026-09-16 18:13:11 +0600

    Hide proxy entry points

--- client/app/modules/workflow/components/node-library-panel/node-definitions.tsx
+++ client/app/modules/workflow/components/node-library-panel/node-definitions.tsx
@@ -126,6 +126,7 @@ export const NodeDefinitions: WorkflowNodeDefinition[] = [
 		category: "action",
 		version: "v1",
 		defaultName: "Proxy",
+		isComingSoon: true,
 		handleSpec: {
 			source: ["source"],
 			target: ["target"],
